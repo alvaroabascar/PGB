c4f0adf183ae7d8e5b05f12166650f564d960d50
Josep Arús <josep@joseparus.com>
2014-11-13 18:56:01 +0100

    fixed r script

--- plots/boxplot.r
+++ plots/boxplot.r
@@ -1,9 +1,19 @@
+#!/usr/bin/env Rscript
 
 # all data
 data = read.csv("../genes/tissue_specific.txt", header=TRUE, sep="\t", skip=1)
 
 # all quantitative variables
-num.data = subset(data, select=c(-Gene, -Protein)) 0.000000001
+num.data = subset(data, select=c(-Gene, -Protein)) + 0.0000001
+
+if(Sys.info()['sysname'] == "Darwin") {
+  quartz()
+} else {
+  x11()
+}
 
 # box plot
 boxplot(num.data, log="y")
+
+message("Press Return To Continue")
+invisible(readLines("stdin", n=1))

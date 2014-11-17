fd4d3203072c09e6a237ddf27e95ddf10bb1600e
Álvaro <alvaroabascar@gmail.com>
2014-11-17 12:21:41 +0100

    compare dnds, boxplot+hists

--- plots/dnds_cmp.r
+++ plots/dnds_cmp.r
@@ -1,23 +1,32 @@
-all <- read.table('../dndsratio_results/allgenes_dndsratio.txt')
-hk <- read.table('../dndsratio_results/housekeeping_dndsratio.txt')
-ts <- read.table('../dndsratio_results/tissue_specific_dndsratio.txt')
+all <- read.table('../genes/dnds/allgenes_dndsratio.txt')
+hk <- read.table('../genes/dnds/housekeeping_dndsratio.txt')
+ts <- read.table('../genes/dnds/tissue_specific_dndsratio.txt')
 
 all <- all$V3
 hk <- hk$V3
 ts <- ts$V3
 
-png('dNdS_mouse.png')#, height=6, width=6, units='in', res=100)
-
-par(mfrow=c(3,1))
-
-xlab <- "dN/dS ratio"
 
 col1 <- rgb(0.6, 0.6, 1)
 col2 <- rgb(0.6, 1, 0.6)
 col3 <- rgb(1, 0.6, 0.6)
 
+png('dNdS_hists.png', height=6, width=6, units='in', res=100)
+par(mfrow=c(3,1))
+xlab <- "dN/dS ratio"
+
 hist(all, nclass=50, col=col1, xlab=xlab, main="All genes")
 hist(hk, nclass=50, col=col2, xlab=xlab, main="Housekeeping genes")
 hist(ts, nclass=50, col=col3, xlab=xlab, main="Tissue-specific genes")
+
+dev.off()
+
+png('dNdS_boxplot.png', height=6, width=6, units='in', res=100)
+
+ylab <- "dN/dS ratio"
+xlabels <- c("All", "Housekeeping", "Tissue specific")
+boxplot(all, hk, ts, names=xlabels, ylab=ylab, col=c(col1, col2, col3),
+        outcol="#333333", pch=18, main="dN/dS ratios across the three datasets")
+
 
 dev.off()

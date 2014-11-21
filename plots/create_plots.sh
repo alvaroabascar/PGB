53e200b5baf3490985836660a44dc6d43b6c79e6
Álvaro <alvaroabascar@gmail.com>
2014-11-21 01:31:51 +0100

    added plots

--- plots/create_plots.sh
+++ plots/create_plots.sh
@@ -1,6 +1,11 @@
 R CMD BATCH boxplot_expr_all.r
+echo "1 done"
 R CMD BATCH boxplot_expr_hk.r
+echo "2 done"
 R CMD BATCH boxplot_expr_ts_nmost.r
+echo "3 done"
 R CMD BATCH boxplot_expr_ts.r
+echo "4 done"
 R CMD BATCH ncrna.r
+echo "5 done"
 rm *Rout

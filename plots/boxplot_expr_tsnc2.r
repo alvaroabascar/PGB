69ea7feb1942a8562314c96dcccda867a376a2e9
Álvaro <alvaroabascar@gmail.com>
2014-11-23 14:09:20 +0100

    data + 1e-5

--- plots/boxplot_expr_tsnc2.r
+++ plots/boxplot_expr_tsnc2.r
@@ -13,15 +13,17 @@ out_file = "expressions/boxplot_expr_ts_nc.png"
 data = read.csv(genes_path, header=TRUE, sep="\t", skip=1)
 
 # all quantitative variables, transformed to decimal logarithm
-num.data = subset(data, select=c(-Gene, -Protein)) + 1e-7
 
-# if(Sys.info()['sysname'] == "Darwin") {
-#   quartz()
-# } else {
-#   x11()
-# }
+min_expr = 1e-5
+num.data = subset(data, select=c(-Gene, -Protein)) + min_expr
 
-png(filename=out_file, height=6, width=6, units='in', res=300)
+if(Sys.info()['sysname'] == "Darwin") {
+  quartz()
+} else {
+  x11()
+}
+
+# png(filename=out_file, height=6, width=6, units='in', res=300)
 # down, left, up, right margins
 par(mar=c(12, 5.1, 4.1, 2.1))
 
@@ -40,6 +42,7 @@ plot = ggplot(data, aes(x=Tissue, y=Expression)) +
        theme(axis.text.x = element_text(angle=45, hjust=1)) +
        xlab("Tissues") +
        ylab("Expressions") +
+       # ylim(c(0,3))
        scale_y_log10(labels=trans_format("log10", math_format(10^.x)))
 
 pvalue_color = function(tissue) {
@@ -53,10 +56,11 @@ pvalue_color = function(tissue) {
 }
 
 tissues = unique(data$Tissue)
-values = sapply(tissues, pvalue_color, simplify=FALSE, USE.NAMES=TRUE)
+# values = sapply(tissues, pvalue_color, simplify=FALSE, USE.NAMES=TRUE)
+values = c('red', 'green', rep('red', 18))
 plot = plot + scale_fill_manual(values=values, guide=FALSE)
 print(plot)
 
-dev.off()
+# dev.off()
 # message("Press Return To Continue")
 # invisible(readLines("stdin", n=1))
